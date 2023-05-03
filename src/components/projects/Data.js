da5b63e7ceede4437d086f91ba8667733ec592c1
Prathipan <prathipan24p@gmail.com>
2023-05-03 15:23:44 +0530

    updated project buttons

--- src/components/projects/Data.js
+++ src/components/projects/Data.js
@@ -12,50 +12,76 @@ export const projects = [
         image: Work4,
         title: "Shop Zone(Ecomm App)",
         category: "MERN",
-        link : "https://shopzone-client.netlify.app/"
+        desc : `Developed a user-friendly online
+        shopping platform with comprehensive product catalogue, secure
+        payment gateway integration, and efficient order management
+        system.`,
+        link : "https://shopzone-client.netlify.app/",
+        FECode: "https://github.com/Prathipan/ecomm-client",
+        BECode : "https://github.com/Prathipan/ecomm-server"
       },
       {
         id: 2,
-        image: Work2,
-        title: "Library management system",
-        category: "React",
-        link : "https://prathipan-lms.netlify.app/"
+        image: Work1 ,
+        title: "Money manager",
+        desc : `Created Money manager where the registered user can track their
+        income and expense histories for future reference.`,
+        category: "MERN",
+        link : "https://prathipan-money-manager.netlify.app/",
+        FECode: "https://github.com/Prathipan/money-manager-client",
+        BECode : "https://github.com/Prathipan/money-manager-backend"
       },
       {
         id: 3,
-        image: Work3,
-        title: "Admin dashboard",
-        category: "React",
-        link : "https://prathipan-sb-admin.netlify.app/portal/dashboard"
+        image: Work5,
+        title: "Chat Application",
+        desc : `Developed a chat application that allows real-time communication
+        between users with message history.`,
+        category: "MERN",
+        link : "https://prathipan-chat-application.netlify.app/",
+        FECode: "https://github.com/Prathipan/Chat-App-Client",
+        BECode : "https://github.com/Prathipan/Chat-App-server"
       },
       {
         id: 4,
-        image: Work1 ,
-        title: "Money manager",
+        image: Work6,
+        title: "Netflix clone",
+        desc : `Implemented a Netflix clone that offers a similar user
+        interface like netflix, personalized content recommendations, and multi-device
+        compatibility for streaming movies.`,
         category: "MERN",
-        link : "https://prathipan-money-manager.netlify.app/ "
+        link : "https://video-clone-client.netlify.app/",
+        FECode: "https://github.com/Prathipan/netflix-clone-client",
+        BECode : "https://github.com/Prathipan/netflix-clone-admin"
       },
       {
         id: 5,
-        image: Work5,
-        title: "Chat Application",
+        image: Work7,
+        title: "Netflix clone admin",
+        desc : `Admin panel for netflix clone where admin can manage the users and videos`,
         category: "MERN",
-        link : "https://prathipan-chat-application.netlify.app/"
-      },
+        link : "https://onlineott-admin-dashboard.netlify.app/",
+        FECode: "https://github.com/Prathipan/netflix-clone-admin"
+      },    
       {
         id: 6,
-        image: Work6,
-        title: "Netflix clone",
-        category: "MERN",
-        link : "https://video-clone-client.netlify.app/"
+        image: Work3,
+        title: "Admin dashboard",
+        category: "React",
+        desc : "Simple admin dashboard with some charts, buttons and users list",
+        link : "https://prathipan-sb-admin.netlify.app/",
+        FECode: "https://github.com/Prathipan/SB-Admin-react"
       },
       {
         id: 7,
-        image: Work7,
-        title: "Netflix clone admin",
-        category: "MERN",
-        link : "https://onlineott-admin-dashboard.netlify.app/"
-      }    
+        image: Work2,
+        title: "Library management system",
+        desc : `Simple Library app which I have implemented simple CRUD operation`,
+        category: "React",
+        link : "https://prathipan-lms.netlify.app/",
+        FECode: "https://github.com/Prathipan/LMS-client"
+      }
+      
 ]
 
 export const projectsNav = [
--- src/components/projects/ProjectItems.js
+++ src/components/projects/ProjectItems.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import "./project.css"
+import React from "react";
+import "./project.css";
 
-const ProjectItems = ({item}) => {
+const ProjectItems = ({ item }) => {
   return (
-    <div className='work-card' key={item.id}>
+    <div className="work-card" key={item.id}>
       <img src={item.image} alt={item.title} className="work-img" />
-      <h3 className='work-title'>{item.title}</h3>
-      <h4>{item.category}</h4>
-      <a href={item.link} className='work-button' target="_blank">Demo<i className="uil uil-arrow-right work-button-icon"></i> </a>
-    </div>
-  )
-}
+      <div>
 
-export default ProjectItems
+      <h3 className="work-title">{item.title}</h3>
+      <h4>{item.category}</h4>
+      </div>
+      <span>{item.desc}</span>
+      <div className="link-buttons">
+        <a href={item.FECode} className="code-button" target="_blank" rel="noreferrer">Frontend code</a>
+        <a href={item.link} className="work-button" target="_blank" rel="noreferrer">
+          Demo<i className="uil uil-arrow-right work-button-icon"></i>{" "}
+        </a>
+        {item.BECode ? <a href={item.BECode} className="code-button" target="_blank" rel="noreferrer">Backend code</a> : <></>}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItems;
